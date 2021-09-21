cdffb6518e399de8a09553d95323dbc7a18c15dc
Hüseyin <huseyin@siyahkare.com>
2021-09-21 17:50:15 +0300

    moonlight

---
+++ application/logs/log-2021-09-21.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2021-09-21 14:35:57 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:35:57 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:35:57 --> Unable to connect to the database
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Unable to connect to the database
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Unable to connect to the database
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Unable to connect to the database
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Unable to connect to the database
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:36:16 --> Unable to connect to the database
+ERROR - 2021-09-21 14:38:39 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:38:39 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:38:39 --> Unable to connect to the database
+ERROR - 2021-09-21 14:38:40 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:38:40 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:38:40 --> Unable to connect to the database
+ERROR - 2021-09-21 14:48:24 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:48:24 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: nodename nor servname provided, or not known /Users/huseyincetinkoz/code/moonlight/system/database/drivers/mysqli/mysqli_driver.php 201
+ERROR - 2021-09-21 14:48:24 --> Unable to connect to the database
